4142a2ea170696405cb26890168d52bf044e6967
Cristina.Diaz <cristina.diaz@ricoh.es>
2021-06-09 12:36:21 +0200

    Se arregla método update

--- src/main/java/com/pruebaSpringBoot/superHeroes/controller/SuperHeroesController.java
+++ src/main/java/com/pruebaSpringBoot/superHeroes/controller/SuperHeroesController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pruebaSpringBoot.superHeroes.entity.SuperHeroe;
@@ -46,8 +47,8 @@ public class SuperHeroesController {
     }
     
     @PutMapping("/superHeroes/{id}")
-    public SuperHeroe update(@PathVariable SuperHeroe superHeroe) throws NotFoundException  {
-
+    public SuperHeroe update(@RequestBody SuperHeroe superHeroe, @PathVariable Long id) throws NotFoundException  {
+    	superHeroe.setId(id);
         return superHeroesService.update(superHeroe);
     }
 }
